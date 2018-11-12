06e029cba78b80ab674823a247501ef56ec311b6
William Maia <josewilliammaia@gmail.com>
2018-11-12 14:01:28 -0300

    correções

--- README.md
+++ README.md
@@ -1,5 +1,13 @@
 # NPLTest
 
 ## run
-**teste**
 docker-compose up
+
+## routes
+
+  **GET** 0.0.0.0:3000
+  **GET** 0.0.0.0:3000/question?q=PARAM
+  **POST** 0.0.0.0:3000/train (with body param 'trainings')
+
+
+git clone git@github.com:WilliamMaia/NPLTest.git
--- app/index.js
+++ app/index.js
@@ -9,24 +9,41 @@ var trainingsFiles = fs.readdirSync('./train');
 
 trainingsFiles.forEach((trainfile) => {
   let trainings = JSON.parse(fs.readFileSync('./train/'+trainfile, 'utf8'));
-  trainings.forEach((train) => classifier.addDocument(train.question, train.result))
+  trainings.forEach((train) => classifier.addDocument(train.question, JSON.stringify(train.result)))
 })
 classifier.train();
 // console.log(classifier.getClassifications(urlParse.query.q));
-// res.end(JSON.stringify(classifier.classify(urlParse.query.q)));
-
 
 var app = express();
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json())
 
 app.get("/question", function (request, response) {
-  response.send(JSON.stringify(classifier.classify(request.query.q)));
+  // console.log(JSON.parse(classifier.classify(request.query.q)));
+  response.send(classifier.classify(request.query.q));
 });
 
 app.post("/train", function (request, response) {
-  console.log('Incoming webhook: ' + JSON.stringify(request.body));
+  // console.log('Incoming webhook: ' + JSON.stringify(request.body));
+  try{
+    let date = new Date();
+    fs.writeFileSync('./train/'+('train'+date.toISOString())+'.json', request.body.trainings);
+  }catch (e){
+    console.log("Cannot write file ", e);
+  }
   response.sendStatus(200);
+});
+
+app.get("/", function (request, response) {
+  let options = {
+    root: __dirname + '/',
+    dotfiles: 'deny',
+    headers: {
+        'x-timestamp': Date.now(),
+        'x-sent': true
+    }
+  };
+  response.sendFile('index.html', options);
 });
 
 var listener = app.listen(3000, function () {
